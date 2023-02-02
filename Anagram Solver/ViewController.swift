40f9142c3755493a41d1cf2b11da775974a1f2bb
Dennis <izuchukwudennis@gmail.com>
2023-02-02 15:50:32 +0300

    Initial Commit

---
+++ Anagram Solver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anagram Solver
+//
+//  Created by Izuchukwu Dennis on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
